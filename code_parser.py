6dee97d94bfe029ad68fafb3c8aab843f06a2e02
ben-23-96 <benspeakman123@gmail.com>
2023-07-01 23:35:06 +0100

    add class and method functionality

--- code_parser.py
+++ code_parser.py
@@ -1,56 +1,162 @@
 from redbaron import RedBaron
 from os import makedirs
-from argparse import ArgumentError
 
 class CodeParser:
-    def __init__(self, filename):
+    def __init__(self, filename: str, function_names: list = [], class_names: list = [], method_names: list = []):
         self.filename = filename
-        self.function_names_list = []
-        self.original_functions_code_list = []
-        self.function_not_found_list = []
+        self.function_names = function_names
+        self.class_names = class_names
+        self.method_names = method_names
+        self.found_function_names_list = []
+        self.found_functions_code_list = []
+        self.not_found_list = []
 
-    def get_target_functions_code(self, function_names: list):
+    def get_target_functions_code(self):
+        # open file and parse the contents 
         with open(self.filename, "r") as source_code:
-            red = RedBaron(source_code.read())
+            parser = RedBaron(source_code.read())
+        
+        # find the classes methods and functions 
+        if self.class_names:
+            self.find_classes(parser)
+        if self.method_names:
+            self.find_methods(parser)
+        if self.function_names:
+            self.find_functions(parser)
+        
+        # return the found functions as strings in a list and the names of the functions not found in another list 
+        return self.found_functions_code_list, self.not_found_list
     
-        for function_name in function_names:
-            function_node = red.find('def', function_name)
-            if not function_node:
-                self.function_not_found_list.append(function_name)
+    def find_classes(self, parser):
+        for class_name in self.class_names:
+            # find the class
+            class_node = parser.find('class', class_name)
+            if not class_node:
+                self.not_found_list.append(class_name)
                 continue
-            self.function_names_list.append(function_node.name)
-            self.original_functions_code_list.append(function_node.dumps())
+            # find all methods in the class
+            method_nodes = class_node.find_all('def')
+            # create a list of all method names as ClassName.method
+            method_names = [f'{class_name}.{method_node.name}' for method_node in method_nodes]
+            # create a list of the code of each method as a string
+            methods_code = [method_node.dumps() for method_node in method_nodes]
+            # add found function names
+            self.found_function_names_list.extend(method_names)
+            # add found code strings
+            self.found_functions_code_list.extend(methods_code)
 
-        if len(self.original_functions_code_list) == 0:
-            function_names_str = (', ').join(function_names)
-            raise Exception(f'Unable to find any of the given functions in {self.filename}, the functions you provided were {function_names_str}')
-    
-        return self.original_functions_code_list, self.function_not_found_list
+    def find_methods(self, parser):
+        for class_method_name in self.method_names:
+            # split ClassName.method to ClassName and method
+            class_name, method_name = class_method_name.split('.')
+            # find the class node in the file
+            class_node = parser.find('class', class_name)
+            # if not found add method to not found list
+            if not class_node:
+                self.not_found_list.append(class_method_name)
+                continue
+            # find the method on the class
+            method_node = class_node.find('def', method_name)
+            # if not found add method to not found list
+            if not method_node:
+                self.not_found_list.append(class_method_name)
+                continue
+            # add found function names to list
+            self.found_function_names_list.append(class_method_name)
+            # add found function code strings to list
+            self.found_functions_code_list.append(method_node.dumps())
+
+    def find_functions(self, parser):
+        # loop over the names of functions you want the code for 
+        for function_name in self.function_names:
+            # find the function
+            function_node = parser.find('def', function_name)
+            # if not found add to not found list and continue
+            if not function_node:
+                self.not_found_list.append(function_name)
+                continue
+            # add the name of the function to the found functions list
+            self.found_function_names_list.append(function_name)
+            # add the code of the function as a str to a list
+            self.found_functions_code_list.append(function_node.dumps())
     
     def replace_target_functions_with_new_functions(self, new_functions: list):
+        # parse the file
         with open(self.filename, 'r') as source_code:
-            red = RedBaron(source_code.read())
+            parser = RedBaron(source_code.read())
+        # create zip containing the name of the function or method and the newly edited version of it as a str
+        functions_name_code_zip = zip(self.found_function_names_list, new_functions)
         
-        functions_code_name_zip = zip(self.function_names_list, new_functions)
+        for name, function_code in functions_name_code_zip:
+            if '.' in name:
+                # must be class method
+                class_name, method_name = name.split('.')
+                # find the class
+                class_node = parser.find('class', class_name)
+                new_method = RedBaron(function_code)
+                # replace the method in the parser object with the newly edited version of the method
+                class_node.find('def', method_name).value = new_method[0].value.dumps().rstrip()
+            else:
+                # must be function
+                # parse the new function code
+                new_function = RedBaron(function_code).find('def', name)
+                # replace the function in the parser object with the newly edited version of the function
+                parser.find('def', name).value = new_function.value.dumps().rstrip()
         
-        for func_tuple in functions_code_name_zip:
-            function_name = func_tuple[0]
-            function_code = RedBaron(func_tuple[1]).find('def', function_name)
-            red.find('def', function_name).replace(f'{function_code}\n\n')
-        
+        # write the edited file content from the parser into the file
         with open(self.filename, 'w') as source_code:
-            source_code.write(red.dumps())
+            source_code.write(parser.dumps())
     
     def create_review_code_files(self, new_functions: list):
+        # make folder to store created files
         makedirs('gpt_function_review', exist_ok=True)
-        functions_zip = zip(self.function_names_list, self.original_functions_code_list, new_functions)
-        for func_tuple in list(functions_zip):
-            function_name = func_tuple[0]
-            original_function = func_tuple[1]
-            new_function = func_tuple[2]
-            
+        # create zip of function/method name, the original function code, and the newly edited function code
+        functions_zip = zip(self.found_function_names_list, self.found_functions_code_list, new_functions)
+        # create list to store the methods for a class if the whole class was provided in as a --target-class
+        classes_name_code_zip_list= []
+        for function_name, original_code, new_code in functions_zip:
+            # if class method ClassName.method
+            if '.' in function_name:
+                # in ClassName in the class names provided
+                if function_name.split('.')[0] in self.class_names:
+                    # add ClassName.method and new function code to list
+                    classes_name_code_zip_list.append((function_name, new_code))
+                    continue
+            # write the original code and new code to a file for comparison
             with open(f'gpt_function_review/{function_name}.py', "w") as f:
                 f.write('##### NEW FUNCTION CODE #####\n\n')
-                f.write(new_function)
+                f.write(new_code)
                 f.write('\n\n##### ORIGINAL FUNCTION CODE #####\n\n')
-                f.write(original_function)
+                f.write(original_code)
+            # call method that will write full classes to file for comparison
+            self.create_class_code_review_file(classes_name_code_zip_list)
+
+    def create_class_code_review_file(self, classes_names_code_list):
+        # parse th file
+        with open(self.filename, "r") as source_code:
+            parser = RedBaron(source_code.read())
+        # loop through given classes
+        for class_name in self.class_names:
+            # group all ClassName.methods for given ClassName into list
+            class_tuples = [name_new_code_tuple for name_new_code_tuple in classes_names_code_list if name_new_code_tuple[0].split('.')[0] == class_name]
+            # find the class in parser object
+            class_node = parser.find('class', class_name)
+            # original class code as string
+            original_class_code = class_node.dumps()
+            for name, new_code in class_tuples:
+                new_method = RedBaron(new_code)
+                # replace method with newly edited method in the parser
+                class_node.find('def', name.split('.')[1]).value = new_method[0].value.dumps().rstrip()
+            # new class code as string
+            new_class_code = class_node.dumps()
+            # create output string
+            output_str = '##### NEW CLASS CODE #####\n\n'
+            output_str += new_class_code
+            output_str += '\n\n##### ORIGINAL CLASS CODE #####\n\n'
+            output_str += original_class_code
+            # write original class and new class to file for comparison
+            with open(f'gpt_function_review/{class_name}.py', "w") as f:
+                f.write(output_str)
+
+    def remove_not_found(self):
+        self.class_names = [class_name for class_name in self.class_names if not class_name in self.not_found_list]
--- main.py
+++ main.py
@@ -11,7 +11,11 @@ def main():
     parser.add_argument('filename', type=str,
                         help='The filename of the file containing the code to be used.')
     parser.add_argument('--target-functions', type=str, nargs='*', default=[],
-                        help='A space-separated list of function names to be targeted for refactoring. ## CLASSES ##')
+                        help='A space-separated list of function names to be targeted.')
+    parser.add_argument('--target-classes', type=str, nargs='*', default=[],
+                        help='A space-separated list of class names to be targeted. Should be provied as ClassName, targets every method in each class provided. Cannot be used if --target-methods also used on a method within one of the given classes.')
+    parser.add_argument('--target-methods', type=str, nargs='*', default=[],
+                        help='A space-separated list of method names to be targeted for refactoring. Each method should be given as ClassName.method. Cannot be used if --target-classes also used on the class the method is from.')
     parser.add_argument('--refactor', action='store_true',
                         help='If set, refactor the code.')
     parser.add_argument('--comments', action='store_true',
@@ -31,30 +35,49 @@ def main():
 
     argument_valiadator = ArgumentValidator(args)
 
-    code_parser = CodeParser(filename=args.filename)
+    code_parser = CodeParser(filename=args.filename, function_names=args.target_functions, class_names=args.target_classes, method_names=args.target_methods)
 
     gpt_request = GptRequest(gpt_4=args.gpt_4)
     
     try:
+        # check arguements fit the requirements
         argument_valiadator.validate()
-
-        functions_code_list, functions_not_found_list = code_parser.get_target_functions_code(function_names=args.target_functions)
     except Exception as e:
         print(e)
         return
-
-    if len(functions_not_found_list) > 0:
-        functions_not_found_str = (', ').join(functions_not_found_list)
-        user_continue = input(f'In {args.filename} unable to find functions: {functions_not_found_str}.\n Do you wish to continue with the functions that were found successfully ? y/n  ')
-        if user_continue != 'y':
+    
+    # find the code as str of the required functions, methods, classes
+    functions_code_list, not_found_list = code_parser.get_target_functions_code()
+    
+    # if some functions were not found
+    if len(not_found_list) > 0:
+        # if no functions were found print message and return
+        if len(functions_code_list) == 0:
+            function_names_str = (', ').join(args.target_functions)
+            class_names_str = (', ').join(args.target_classes)
+            method_names_str = (', ').join(args.target_methods)
+            print(f'Unable to find any of the given functions, methods or classes in {args.filename}.\nFunctions provided: {function_names_str}\nMethods provided: {method_names_str}\nClasses provided: {class_names_str}')
             return
-
+        # some functions were found display functions not found to user ask if to continue
+        not_found_str = (', ').join(not_found_list)
+        user_continue = input(f'In {args.filename} unable to find: {not_found_str}.\n Do you wish to continue with the functions that were found successfully ? y/n  ')
+        if user_continue == 'y':
+            # if continue remove not found items in class attributes
+            code_parser.remove_not_found()
+        else:
+            # if not continue return
+            return
+    
+    # create prompts message strings to be send to gpt api
     gpt_request.create_prompts(functions=functions_code_list, refactor=args.refactor, comments=args.comments, docstrings=args.docstrings, error_handling=args.error_handling)
     
+    # send the requests to the api return the newly edited functions as strings
     new_functions = gpt_request.make_GPT_requests()
     if args.create_review_file:
+        # create a file to view the original function or class and the gpt edited one for comparison
         code_parser.create_review_code_files(new_functions=new_functions)
     if args.edit_code_in_file:
+        # edit the code in the file replacing the class or function or method with the gpt edited one
         code_parser.replace_target_functions_with_new_functions(new_functions=new_functions)
 
 
